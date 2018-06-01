fedfba0609ebfbb74a7667acd3c2546166d5a7b1
Babak <badaei@yahoo.com>
2018-05-31 19:35:26 -0700

    added CLI tests

--- test/sample-query.js
+++ test/sample-query.js
@@ -1,5 +1,7 @@
 import test from 'ava';
 import getQuery from "../lib/get-query"
+import childProcess from "child_process";
+import path from 'path'
 
 const expectedOutput = `
 select json_build_object(
@@ -52,9 +54,28 @@ const query = {
   } 
 }
 
-// TODO: Tets
+const basePath = process.cwd()
+const jsonQueryPath = `${basePath}/test/query.json`
+const yamlQueryPath = `${basePath}/test/query.yaml`
+const cliPath = `${basePath}/bin/pg-nested-json-query`
+
+
 test('Test sample query', t => {
 	t.deepEqual(getQuery(query).trim(), expectedOutput)
+});
+
+test('Test sample JSON query with CLI using `/usr/bin/env node`', t => {
+	t.deepEqual(
+    childProcess.execSync(`/usr/bin/env node ${ cliPath } ${ jsonQueryPath }`).toString().trim(), 
+    expectedOutput
+  )
+});
+
+test('Test sample YAML query with CLI using `/usr/bin/env node`', t => {
+	t.deepEqual(
+    childProcess.execSync(`/usr/bin/env node ${ cliPath } ${ yamlQueryPath }`).toString().trim(), 
+    expectedOutput
+  )
 });
 
 
